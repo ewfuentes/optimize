68354c2f2de0b731ade09b8f5a749cf68dbdb75f
Erick Fuentes <fuentes.erick@gmail.com>
2018-11-16 07:19:01 -0500

    make clang happy

--- interface/function.hh
+++ interface/function.hh
@@ -1,29 +1,41 @@
+#pragma once
+
 #include <Eigen/Core>
 
 namespace optimize {
 template <int SIZE> struct Function {
   using Vector = Eigen::Matrix<double, SIZE, 1>;
+  static constexpr double DIMENSION = SIZE;
 
   // Evaluate the function at the specified point
-  virtual double operator()(const Vector &x);
+  virtual double operator()(const Vector &x) const = 0;
 };
 
 template <int SIZE> struct DifferentiableFunction : public Function<SIZE> {
   using Vector = typename Function<SIZE>::Vector;
-  virtual Vector gradient(const Vector &x);
+  virtual Vector gradient(const Vector &x) const = 0;
 };
 
 template <int SIZE>
 struct TwiceDifferentiableFunction : public DifferentiableFunction<SIZE> {
   using typename Function<SIZE>::Vector;
   using Hessian = Eigen::Matrix<double, SIZE, SIZE>;
-  virtual Hessian hessian(const Vector &x);
+  virtual Hessian hessian(const Vector &x) const = 0;
 };
 
-template <> struct Function<1> { using Vector = double; };
+template <> struct Function<1> {
+  using Vector = double;
+  virtual double operator()(const double &x) const = 0;
+};
 
-template <> struct TwiceDifferentiableFunction<1> : DifferentiableFunction<1> {
+  template <> struct DifferentiableFunction<1> : public Function<1> {
+  using Vector = double;
+  virtual double gradient(const double &x) const = 0;
+};
+
+template <> struct TwiceDifferentiableFunction<1> : public DifferentiableFunction<1> {
   using Hessian = double;
+  virtual double hessian(const double &x) const = 0;
 };
 
 } // namespace optimize
--- interface/function_test.cc
+++ interface/function_test.cc
@@ -5,7 +5,7 @@
 namespace optimize {
 TEST(FunctionTest, test_function) {
   struct TestFunction : public Function<1> {
-    double operator()(const Vector &x) { return x + 1.0; }
+    double operator()(const Vector &x) const override { return x + 1.0; }
   };
   TestFunction x;
   EXPECT_EQ(x(1), 2.0);
@@ -13,26 +13,26 @@ TEST(FunctionTest, test_function) {
 
 TEST(DifferentiableFunctionTest, test_function) {
   struct TestFunction : public DifferentiableFunction<1> {
-    double operator()(const double &x) { return x + 1.0; }
+    double operator()(const double &x) const override { return x + 1.0; }
 
-    double gradient(const double &x) { return 1.0; }
+    double gradient(const double &x) const override { return 1.0; }
   };
   TestFunction x;
   EXPECT_EQ(x(1), 2.0);
   EXPECT_EQ(x.gradient(1.0), 1.0);
 }
 
-  TEST(TwiceDifferentiableFunctionTest, test_function) {
-    struct TestFunction : public TwiceDifferentiableFunction<1> {
-      double operator()(const double &x) { return x*x + 2 * x + 1.0; }
+TEST(TwiceDifferentiableFunctionTest, test_function) {
+  struct TestFunction : public TwiceDifferentiableFunction<1> {
+    double operator()(const double &x) const override { return x * x + 2 * x + 1.0; }
 
-      double gradient(const double &x) { return 2*x + 2; }
+    double gradient(const double &x) const override { return 2 * x + 2; }
 
-      double hessian(const double &x) {return 2; }
-    };
-    TestFunction x;
-    EXPECT_EQ(x(1), 4.0);
-    EXPECT_EQ(x.gradient(1.0), 4.0);
-    EXPECT_EQ(x.hessian(1.0), 2.0);
-  }
+    double hessian(const double &x) const override { return 2; }
+  };
+  TestFunction x;
+  EXPECT_EQ(x(1), 4.0);
+  EXPECT_EQ(x.gradient(1.0), 4.0);
+  EXPECT_EQ(x.hessian(1.0), 2.0);
+}
 } // namespace optimize
